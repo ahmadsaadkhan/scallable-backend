da1ddd0292764c8d6e8ea4ac4131ba485e14b561
asaadcerebra <asaad@cerebra.sa>
2025-05-06 11:46:28 +0100

    port changed

--- app.js
+++ app.js
@@ -7,8 +7,7 @@ dotenv.config();
 const app = express();
 // require('./config/db');
 
-const PORT = process.env.PORT || 5000;
-
+const PORT = process.env.PORT || 8080;
 
 app.use(cors());
 app.use(express.json());
